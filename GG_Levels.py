5ff0f764ada21a4047f42eaf586a153631403cc9
Alex Swift <alex@carlingfordgroup.co.uk>
2016-12-01 21:43:31 +0000

    Add files via upload

--- GG_Levels.py
+++ GG_Levels.py
@@ -1,19 +1,28 @@
 colors = {'a':'pzl:Gray3','b':'pzl:Gray2'}
 
-level1 = '''
-aaaaaaaaaaaa
-a          a
-aaaa    aaaa
-a          a
-aaaaaaaaaaaa
-'''
+level1 = ( ('''aaaaaaaa    '''),
+		('''a     aaa     '''), 
+		('''a.        aaaa'''),
+		('''a aaaaa       a'''),
+		('''aaaaaaaaaaaaa a'''))
 
-level2 = '''
-aaaaaaaaaaaa
 
-aaaaaaaaaaaa
+Levels = []
 
-aaaaaaaaaaaa
-'''
+def LoadLevel( level )
 
-levels = [level1,level2]
+	Blocks = []
+
+	block_w = _S.size.w/(len(level1[1]))
+	block_h = _S.size.h/(len(level1))
+	
+	min_x = block_w/2
+	min_y = block_h/2
+
+	for y, line in enumerate(reversed(level1)):
+		for x, char in enumerate(line):
+			if char == ' ': continue
+			pos = Point(x * block_w + min_x, min_y + y * block_h)
+			block = Block(char, position=pos, parent=self.game_node)
+			block.size = (block_w, block_h)
+			self.blocks.append(block)
--- grin.py
+++ grin.py
@@ -8,22 +8,19 @@ from classes.bullet import bullet
 
 import util.gg_math
 
-from GG_Levels import levels, colors
+from GG_Levels import *
 
 A = Action
 
 #####___GRiM - initial stamina, position and can move
 GRiM_STaMina = 100
 GriM_position = (256,120)
-GRim_can_move = True
-grim = Player(GRiM_STaMina, GriM_position, GRim_can_move, SpriteNode('spc:CockpitRed1'))							
+grim = Player(GRiM_STaMina, GriM_position, SpriteNode('spc:CockpitRed1'))							
 
 #####___GRiN 
 GRin_StaMina = 100
 GRIN_positiON = (140,568)
-GRIn_can_move = False
-grin = Player(GRin_StaMina, GRIN_positiON, GRIn_can_move, SpriteNode('spc:CockpitGreen1') )
-
+grin = Player(GRin_StaMina, GRIN_positiON, SpriteNode('spc:CockpitGreen1') )
 
 
 ##%%## -GG1 -Grin n Grim Test Room ***
@@ -34,22 +31,12 @@ class GG1(Scene):
 	
 		_S = self
 		
-		self.level = 0
-		
 		###Loads Player and Object Sprites
 		self.cursor = SpriteNode('iob:arrow_down_c_32')
 		self.ripple = SpriteNode('shp:wavering')
 		
 		self.button = SpriteNode('plc:Selector')
 		self.button.on = True
-		
-		self.bullet = SpriteNode('spc:LaserBlue13')
-		self.bullet.scale = 0.5
-		self.bullet.speed = 50
-		
-		self.blocks = []
-		
-		self.block = Block in enumerate(self.blocks)
 		
 		self.effect_node = EffectNode(parent=self)
 		self.effect_node.crop_rect = self.bounds
@@ -59,10 +46,7 @@ class GG1(Scene):
 		self.expl = SpriteNode('shp:Explosion03')
 		self.expl.scale = 0.25
 		
-		
-		###Sends to Menu
-		grim.can_move = True
-		grin.can_move = False
+		self.SetActivePlayer( grim )
 		
 		self.Menu()
 	
@@ -72,7 +56,7 @@ class GG1(Scene):
 			player:Update()
 			
 		for k,bullet in Bullets:
-			player:Update()
+			bullet:Update()
 			
 				
 	###Menu Screen
@@ -104,15 +88,12 @@ class GG1(Scene):
 			
 		###Play Setting
 		elif self.gamesetting == 'Play':
-			
-			ply = self.GetActivePlayer()
-			ply:MoveBegin( touch )
+			self.GetActivePlayer():MoveBegin( touch )
 			
 	###TOUCH_WHILE									
 	def touch_moved(self, touch):
 		self.cursor.position = touch.location
-		ply = self.GetActivePlayer()
-		ply:MoveWhile( touch )
+		self.GetActivePlayer():MoveWhile( touch )
 
 	###TOUCH_ENDED
 	def touch_ended(self,touch):
@@ -122,8 +103,7 @@ class GG1(Scene):
 		self.ripple.position = self.target
 		self.add_child(self.ripple)
 		
-		ply = self.GetActivePlayer()
-		ply:MoveEnded( touch )
+		self.GetActivePlayer():MoveEnded( touch )
 					
 	###Start	
 	def GameStart(self):		
@@ -137,40 +117,8 @@ class GG1(Scene):
 		###Buttons And Switches
 		self.button.position = self.size/2
 		self.add_child(self.button)
-		
-
-		level1 = ( ('''aaaaaaaa    '''),
-		('''a     aaa     '''), 
-		('''a.        aaaa'''),
-		('''a aaaaa       a'''),
-		('''aaaaaaaaaaaaa a'''))
-
-		block_w = self.size.w/(len(level1[1]))
-		block_h = self.size.h/(len(level1))
-		
-		min_x = block_w/2
-		min_y = block_h/2
-
-		for y, line in enumerate(reversed(level1)):
-			for x, char in enumerate(line):
-				if char == ' ': continue
-				pos = Point(x * block_w + min_x, min_y + y * block_h)
-				block = Block(char, position=pos, parent=self.game_node)
-				block.size = (block_w, block_h)
-				self.blocks.append(block)
-
-						
-	###Grim Turn Start 	
-	def Grim_Turn(self, grim):
-		self.grim.can_move = True
-		self.grin.can_move = False
-		print( str('Tim') + str(self.grim.stamina))
-
-	###Grin Turn Start
-	def Grin_Turn(self,grin):
-		self.grin.can_move = True		
-		self.grim.can_move = False
-		print(str('n00b')+str(self.grin.stamina))
+	
+		LoadLevel( level1 )
 	
 	def Button_Press(self,button):
 		
@@ -190,5 +138,11 @@ class GG1(Scene):
 		
 	def SetActivePlayer( ply )
 		self.ActivePlayer = ply
-
+		
+	def SwapActivePlayer()
+		if self.ActivePlayer == grin:
+			self.SetActivePlayer( grim )
+		elif:
+			self.SetActivePlayer( grin )
+			
 run(GG1(), LANDSCAPE)
