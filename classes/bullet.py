027f29e24e7fa55919bb793e0e9d25455a98d2dd
Timborob <t.roberts.14@aberdeen.ac.uk>
2016-12-07 17:36:40 +0000

    Update bullet.py

--- classes/bullet.py
+++ classes/bullet.py
@@ -1,25 +1,30 @@
+from scene import SpriteNode
 import math as ma
 
 Bullets = []
 
-class bullet(object):
-	def __init__(self,position,type,rotation,spirit_node):
+class bullet():
+	def __init__(self,position,rotation):
 		self.position = position
 		self.speed = 50
-		self.type = type
+		self.type = 1
 		self.theta = rotation
-		self.spirit_node = spirit_node or SpriteNode('spc:LaserBlue13')
-		self.spirit_node.size = 0.5
+		self.spirit_node = SpriteNode('spc:LaserBlue13')
+		#self.spirit_node.size = 0.5
 		
 		Bullets.append( self )
-		_S.add_child( self.spirit_node )
+		#_S.add_child( self.spirit_node )
 		
-	def Remove( self )
+	def GetAll():
+		return Bullets
+		
+	def Remove( self ):
 		Bullets.remove( self )
 		self.spirit_node.remove_from_parent()
 				
 		
-	def Update( self )
-		self.position = self.position += (ma.cos(self.theta)*self.speed, ma.sin(self.theta)*self.speed)
-		if self.position >= _SCENE.size of self.position <= (0,0):
-			self:Remove()
+	def Update( self , _S):
+		self.position += (ma.cos(self.theta)*self.speed, ma.sin(self.theta)*self.speed)
+		#if self.position >= _S.size or self.position <= (0,0):
+		if not _S.game_node.bbox.contains_point(self.position):
+			self.Remove()
