0d5bd6e7ac6f1ce3be6efe40df0f96e5ea47b9be
Timborob <t.roberts.14@aberdeen.ac.uk>
2016-12-07 17:37:26 +0000

    Update block.py

--- classes/block.py
+++ classes/block.py
@@ -1,14 +1,16 @@
+from scene import SpriteNode
+
 Blocks = []
 
 colors = {'a':'pzl:Gray3','b':'pzl:Gray2'}
 
-class Block(SpriteNode):
+class Block():
 	def __init__(self,block_type, *args, **kwargs):
 		img = colors.get(block_type,'pzl:Blue3')
-		SpriteNode.__init__(self,img,*args,**kwargs)
+		self.spirit_node = SpriteNode(img,*args,**kwargs)
 		self.block_type = block_type
 		Blocks.append(self)
 		
-	def Remove( self )
+	def Remove( self ):
 		Blocks.remove( self )
 		self.spirit_node.remove_from_parent()
